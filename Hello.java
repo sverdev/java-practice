9ef08c08dcd3d51d8f19b172ebfa7a08744e875d
sh4lluloveme <wh3n_gg@kakao.com>
2023-09-03 15:26:03 +0900

    First Commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        String birthday = "3월 4일";
+        String name = "이지혁";
+        System.out.println("안녕하세요! 제 이름은 " + name + "이고, 생일은 " + birthday + " 입니다.");
+    }
+}
